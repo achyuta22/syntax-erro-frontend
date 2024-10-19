91b03e31013013ebbab02680e62c1fce441f1815
RAVIHAL BHARAT ACHYUTA <136910474+achyuta22@users.noreply.github.com>
2024-10-20 03:48:25 +0530

    Update Socket.js

--- src/Utils/Socket.js
+++ src/Utils/Socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
 // Connect to the backend server (change the port if necessary)
-const socket = io('http://43.204.211.130:5000/');
+const socket = io('https://syntax-error.onrender.com/');
 
 export default socket;
