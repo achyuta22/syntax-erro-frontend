e6db34d72399dfb58fb59a76f6fbbbbf42840410
yomanthunder <vemareddy_sr@ee.iitr.ac.in>
2024-10-19 12:21:50 +0530

    feat:port setup

--- src/Utils/Socket.js
+++ src/Utils/Socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
 // Connect to the backend server (change the port if necessary)
-const socket = io('http://localhost:3000');
+const socket = io('http://localhost:4000');
 
 export default socket;
